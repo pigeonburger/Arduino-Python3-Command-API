77ac48eff2cfc1a83f54de527272dd8e7c3b97a4
pigeonburger <70826123+pigeonburger@users.noreply.github.com>
2021-08-11 13:28:09 +1000

    version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
   name="arduino-python3",
-  version="0.7",
+  version="0.8",
   install_requires=['pyserial'],
   author="Pigeonburger",
   author_email="pigeonburger@pigeonburger.xyz",
@@ -13,7 +13,7 @@ setuptools.setup(
   bridge for communicating with Arduino microcontroller boards. Extended to work with Python 3",
   long_description=long_description,
   long_description_content_type="text/markdown",
-  url='https://github.com/pigeonburger/Arduino-Python3-Command-API',
+  url='https://github.com/pigeonburger/pyArduinoAPI',
   packages=['Arduino'],
   license='MIT',
 )
